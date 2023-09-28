c57b8d7ed15540ffe6f3de7b51954e551273e5c7
z4nder <gusttavo212@gmail.com>
2023-09-27 21:52:09 -0300

    wip

--- utils/phrasesManager.ts
+++ utils/phrasesManager.ts
@@ -102,6 +102,98 @@ export default {
       {
         text: "Está tão quente que até os pinguins estão de férias no Caribe.",
       },
+      {
+        text: "O calor está tão forte que até os pássaros estão procurando uma piscina.",
+      },
+      {
+        text: "Hoje está tão quente que os sorvetes estão fugindo do congelador.",
+      },
+      { text: "Parece que o sol decidiu nos abraçar com seu calor hoje." },
+      { text: "O termômetro está subindo mais rápido do que um foguete." },
+      { text: "Está tão quente que até os relógios estão derretendo." },
+      { text: "O calor está mais intenso do que um abraço de urso polar." },
+      {
+        text: "É como se o sol estivesse fazendo um churrasco no quintal hoje.",
+      },
+      { text: "O calor está tão forte que até as estátuas estão suando." },
+      { text: "Hoje o calor está tão intenso que até o gelo está derretendo." },
+      { text: "O verão chegou com tudo e trouxe o calor como acompanhante." },
+      {
+        text: "Se o calor fosse uma música, hoje estaria em um solo de guitarra ardente.",
+      },
+      {
+        text: "Está tão quente que até os corações estão derretendo de paixão.",
+      },
+      {
+        text: "O termômetro está tão alto que parece um foguete pronto para decolar.",
+      },
+      { text: "Hoje o calor está mais quente do que um abraço apertado." },
+      { text: "É como se o sol estivesse mandando um olá em chamas!" },
+      { text: "O calor está tão intenso que até os iglus estão derretendo." },
+      {
+        text: "Se o calor fosse um filme, hoje seria um suspense insuportável.",
+      },
+      { text: "Está tão quente que até os ventos estão assando pães." },
+      {
+        text: "O calor está tão forte que até os ursos polares estão tomando sorvete.",
+      },
+      { text: "Hoje o calor está mais quente do que um vulcão em erupção." },
+      { text: "É como se o sol estivesse batendo palmas de fogo hoje." },
+      {
+        text: "O calor está tão intenso que até os pinguins estão usando óculos de sol.",
+      },
+      { text: "Se o calor fosse um animal, hoje seria um leão rugindo." },
+      { text: "Está tão quente que até os desertos estão invejosos." },
+      { text: "O termômetro está tão alto que parece uma escada para o céu." },
+      { text: "Hoje o calor está mais quente do que um beijo apaixonado." },
+      { text: "É como se o sol estivesse sorrindo com dentes de fogo hoje." },
+      { text: "O calor está tão forte que até os sorvetes estão suando." },
+      {
+        text: "Se o calor fosse uma bebida, hoje seria um coquetel explosivo.",
+      },
+      { text: "Está tão quente que até os pinguins estão fazendo picolé." },
+      {
+        text: "O calor está tão intenso que até os iglus estão virando poças.",
+      },
+      { text: "Hoje o calor está mais quente do que uma panela de pressão." },
+      { text: "É como se o sol estivesse abraçando a terra com chamas hoje." },
+      {
+        text: "O calor está tão forte que até os esquilos estão tomando banho de gelo.",
+      },
+      {
+        text: "Se o calor fosse um carro, hoje seria uma Ferrari em alta velocidade.",
+      },
+      { text: "Está tão quente que até os oceanos estão evaporando." },
+      {
+        text: "O calor está tão intenso que até os picolés estão correndo para a sombra.",
+      },
+      { text: "Hoje o calor está mais quente do que uma fornalha ardente." },
+      { text: "É como se o sol estivesse lançando faíscas de fogo hoje." },
+      {
+        text: "O calor está tão forte que até os pinguins estão usando protetor solar.",
+      },
+      {
+        text: "Se o calor fosse um planeta, hoje seria o mais próximo do sol.",
+      },
+      { text: "Está tão quente que até as pedras estão suando." },
+      {
+        text: "O termômetro está tão alto que parece um míssil prestes a decolar.",
+      },
+      { text: "Hoje o calor está mais quente do que uma paixão ardente." },
+      {
+        text: "É como se o sol estivesse lançando raios de calor por toda parte hoje.",
+      },
+      {
+        text: "O calor está tão intenso que até os ursos polares estão indo à praia.",
+      },
+      {
+        text: "Se o calor fosse uma comida, hoje seria uma pimenta malagueta.",
+      },
+      { text: "Está tão quente que até as árvores estão pedindo água." },
+      {
+        text: "O calor está tão forte que até os pinguins estão usando bermudas.",
+      },
+      { text: "Hoje o calor está mais quente do que um abraço de urso polar." },
     ];
   },
 };
